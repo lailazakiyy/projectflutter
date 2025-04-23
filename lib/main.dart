345d64b4e399d3b1e17f8375a8b02ab5e5fbd9cf
lailazakiyy <itsayeuz@gmail.com>
2025-04-23 22:02:48 +0700

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'login_screen.dart';
+import 'profile_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -8,18 +9,14 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
       ),
-      //home: LoginScreen(),
-      initialRoute: '/login',
-      routes: {'/login': (context) => LoginScreen()},
+      home: const Loginscreen(),
     );
   }
 }
