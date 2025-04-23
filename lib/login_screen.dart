115002a920f8aebaaf755b262c49aae747a41e32
lailazakiyy <itsayeuz@gmail.com>
2025-04-23 22:04:08 +0700

    Update login_screen.dart

--- lib/login_screen.dart
+++ lib/login_screen.dart
@@ -1,126 +1,85 @@
 import 'package:flutter/material.dart';
 import 'profile_screen.dart';
 
-class LoginScreen extends StatelessWidget {
-  final TextEditingController usernameController = TextEditingController();
-  final TextEditingController passwordController = TextEditingController();
+class Loginscreen extends StatefulWidget {
+  const Loginscreen({super.key});
 
-  LoginScreen({Key? key}) : super(key: key);
+  @override
+  State<Loginscreen> createState() => _LoginscreenState();
+}
 
-  void _login(BuildContext context) {
-    String username = usernameController.text;
-    String password = passwordController.text;
+class _LoginscreenState extends State<Loginscreen> {
+  TextEditingController emailcontroller = TextEditingController();
+  TextEditingController passwordcontroller = TextEditingController();
+  bool tampilPassword = true;
 
-    // Cek username dan password (contoh sederhana)
-    if (username == 'user' && password == 'password') {
-      // Jika login berhasil, arahkan ke ProfileScreen dengan informasi pengguna
-      Navigator.push(
-        context,
-        MaterialPageRoute(
-          builder:
-              (context) => ProfileScreen(
-                name:
-                    "Laila Zakiya Ufairatu Z", // Ganti dengan nama yang sesuai
-                contact: "08123456789", // Ganti dengan kontak yang sesuai
-                email: username, // Menggunakan username sebagai email
-                domicile: "Solo", // Ganti dengan domisili yang sesuai
-                status: "Active", // Ganti dengan status yang sesuai
-                isEditable: true, // Tambahkan parameter untuk mengizinkan edit
-              ),
-        ),
-      );
-    } else {
-      // Tampilkan pesan kesalahan
-      ScaffoldMessenger.of(context).showSnackBar(
-        const SnackBar(content: Text('Username atau password salah')),
-      );
-    }
+  void showPassword() {
+    setState(() {
+      tampilPassword = !tampilPassword;
+    });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: const Color(0xFFF4F4F5),
       appBar: AppBar(
+        title: const Text("LOGIN PAGE", style: TextStyle(fontWeight: FontWeight.bold, color: Colors.white),),
         backgroundColor: const Color(0xFF2C3E50),
-        title: const Text(
-          "LOGIN PAGE",
-          style: TextStyle(fontWeight: FontWeight.bold, color: Colors.white),
-        ),
-        centerTitle: true,
       ),
-      body: Center(
-        child: Padding(
-          padding: const EdgeInsets.all(20.0),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Center(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              const CircleAvatar(
-                radius: 60,
-                backgroundColor: Color(0xFFBDC3C7),
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              CircleAvatar(
+                radius: 50,
                 child: Icon(Icons.person, size: 65, color: Color(0xFF34495E)),
               ),
-              const SizedBox(height: 16),
-              const Text(
-                "Login",
-                style: TextStyle(
-                  fontSize: 32,
-                  fontWeight: FontWeight.w600,
-                  color: Color(0xFF2C3E50),
-                ),
+              SizedBox(height: 20),
+              Text(
+                'Login',
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 25),
               ),
-              const SizedBox(height: 20),
+              SizedBox(height: 25),
               TextField(
-                controller: usernameController,
+                controller: emailcontroller,
                 decoration: InputDecoration(
-                  labelText: 'Username',
-                  prefixIcon: const Icon(
-                    Icons.person_2_sharp,
-                    color: Color(0xFF95A5A6),
-                  ),
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(10),
-                    borderSide: const BorderSide(color: Color(0xFFBDC3C7)),
-                  ),
-                  focusedBorder: OutlineInputBorder(
-                    borderSide: const BorderSide(color: Color(0xFF34495E)),
-                    borderRadius: BorderRadius.circular(10),
-                  ),
+                  labelText: 'Email',
+                  hintText: 'Masukkan Email',
+                  prefixIcon: Icon(Icons.person),
+                  border: OutlineInputBorder(),
                 ),
               ),
-              const SizedBox(height: 12),
-              TextFormField(
-                controller: passwordController,
-                obscureText: true,
+              SizedBox(height: 10),
+              TextField(
+                obscureText: tampilPassword,
+                controller: passwordcontroller,
                 decoration: InputDecoration(
                   labelText: 'Password',
-                  prefixIcon: const Icon(Icons.lock, color: Color(0xFF95A5A6)),
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(10),
-                    borderSide: const BorderSide(color: Color(0xFFBDC3C7)),
+                  hintText: 'Masukkan Password',
+                  suffixIcon: IconButton(
+                    onPressed: showPassword,
+                    icon: Icon(tampilPassword ? Icons.visibility_off : Icons.visibility),
                   ),
-                  focusedBorder: OutlineInputBorder(
-                    borderSide: const BorderSide(color: Color(0xFF34495E)),
-                    borderRadius: BorderRadius.circular(10),
-                  ),
+                  prefixIcon: Icon(Icons.lock),
+                  border: OutlineInputBorder(),
                 ),
               ),
-              const SizedBox(height: 24),
+              SizedBox(height: 20),
               ElevatedButton(
-                onPressed: () => _login(context),
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => Profilescreen()),
+                  );
+                },
+                child: Text('Login'),
                 style: ElevatedButton.styleFrom(
-                  backgroundColor: const Color(0xFF3498DB),
-                  padding: const EdgeInsets.symmetric(
-                    horizontal: 40,
-                    vertical: 16,
-                  ),
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(8),
-                  ),
-                ),
-                child: const Text(
-                  "Login",
-                  style: TextStyle(fontSize: 18, color: Colors.white),
+                  backgroundColor: Colors.blue,
+                  padding: EdgeInsets.symmetric(horizontal: 100, vertical: 15),
+                  textStyle: TextStyle(fontSize: 16),
                 ),
               ),
             ],
